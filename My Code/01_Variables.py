8b268526d719810df84dfceba83deef33d83cb58
Njengaaa <collins.njenga@strathmore.edu>
2026-04-20 12:09:32 +0300

    Loops

--- My Code/01_Variables.py
+++ My Code/01_Variables.py
@@ -8,6 +8,16 @@ flag = True
 #print(type(balance))
 #print(type(flag))
 
-for i in range(5):
-    print(i)
+# """
+# for i in range(2,18,2):
+#     print(i)
 
+# items = [1,2,3,4,5,6,7,8,9]
+# for item in items:
+#     print(item)
+    
+
+    
+for i in range(18):
+    if i == 17:
+        print(i)
